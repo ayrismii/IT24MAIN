5d27543ef504c814c2a179c224b318caa763f7d6
ayrismii <20231434@nbsc.edu.ph>
2024-10-11 09:44:54 +0800

    initialization

--- applet-4.js
+++ applet-4.js
@@ -5,3 +5,9 @@ class StudentList {
         this.students = [];
         this.init();
     }
+    async init() {
+        await this.fetchData();
+        this.renderStudentList(this.students); 
+        this.bindSearchEvent();
+    }
+
